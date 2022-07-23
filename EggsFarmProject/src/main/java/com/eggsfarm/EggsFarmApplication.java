8d31584a19335bf0d3b2019123f61f37702ac8ad
Dennys Morillo <dennyoscar13@gmail.com>
2022-07-23 09:47:29 -0400

    Backend de la aplicacion

---
+++ EggsFarmProject/src/main/java/com/eggsfarm/EggsFarmApplication.java
@@ -0,0 +1,13 @@
+package com.eggsfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggsFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggsFarmApplication.class, args);
+	}
+
+}
---
+++ EggsFarmProject/src/test/java/com/eggsfarm/EggsFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eggsfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggsFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
